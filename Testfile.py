66688d8356ce0c3baaf8be83b94b3ff202b6c486
apatil210 <apatil2@lbl.gov>
2026-06-01 12:41:00 -0700

    Update Testfile.py

--- Testfile.py
+++ Testfile.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 pio.templates.default = "plotly"
 
-DATA_URL = "hhttps://raw.githubusercontent.com/apatil210/LDRD2/main/Modified%20Data.xlsx"
+DATA_URL = "https://raw.githubusercontent.com/apatil210/LDRD2/main/Modified%20Data.xlsx"
 THRESHOLD_PCT = 1.0
 
 TEXT_COLOR = "#14212B"
