78cb1631537f98502b05fa3477442fa9ddcdd6c7
apatil210 <apatil2@lbl.gov>
2026-06-02 11:12:44 -0700

    Update Testfile.py

--- Testfile.py
+++ Testfile.py
@@ -304,8 +304,8 @@ try:
             st.subheader(f"Fact Sheet: {selected_process}")
 
             c1, c2 = st.columns(2)
-            c1.metric("Annual Production", f"{fact_sheet['Annual Production']:.2f}")
-            c2.metric("Annual Energy", f"{fact_sheet['Annual Energy']:.2f}")
+            c1.metric("Annual Production (tonne/yr)", f"{fact_sheet['Annual Production']:.2f}")
+            c2.metric("Annual Energy(PJ/yr)", f"{fact_sheet['Annual Energy']:.2f}")
 
             st.caption(f"Underlying rows used: {fact_sheet['Rows']}")
 
