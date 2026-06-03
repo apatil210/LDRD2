0006651d6a4fb52b5f15e2ace13919bec4f00a47
apatil210 <apatil2@lbl.gov>
2026-06-03 11:05:00 -0700

    Update Testfile2.py

--- Testfile2.py
+++ Testfile2.py
@@ -250,7 +250,7 @@ def build_fact_sheet(df: pd.DataFrame, selected_l2: str):
             residence_time_col,
         ]
     ].rename(columns={
-        l3_col: "Unit Operations",
+        l3_col: "Industrial Process",
         sec_elec_col: "SEC Electricity",
         sec_fuels_col: "SEC Fuels",
         sec_steam_col: "SEC Steam",
