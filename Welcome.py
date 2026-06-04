60f7f09cd132ff5e26c1ecdd495f955cd4944a75
apatil210 <apatil2@lbl.gov>
2026-06-04 12:34:55 -0700

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -10,7 +10,7 @@ contributors = [
     {
         "name": "Akash Patil",
         "title": "Postdoctoral Researcher · Energy Analysis Division",
-        "photo": "https://github.com/apatil210/LDRD2/blob/main/Akashpic1.JPG",
+        "photo": "https://raw.githubusercontent.com/apatil210/LDRD2/main/Akashpic1.JPG",
     },
     {
         "name": "M. Jibran S. Zuberi",
